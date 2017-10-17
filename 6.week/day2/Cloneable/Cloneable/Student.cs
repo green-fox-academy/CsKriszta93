6f53b2e6a2b6dcbc68ed1ecd67337696e38d7585
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-17 16:09:17 +0200

    Student Class

---
+++ 6.week/day2/Cloneable/Cloneable/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloneable
+{
+    public interface ICloneable
+    {
+        object Clone();
+    }
+
+    class Student : ICloneable
+    {
+        public string name;
+        public int age;
+        public string gender;
+        public string previousOrganization;
+
+        public Student(string name, int age, string gender, string previousOrganization)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.previousOrganization = previousOrganization;
+        }
+
+        public Student()
+        {
+            name = "Jane Doe";
+            age = 20;
+            gender = "male";
+            previousOrganization = "BME";
+        }
+
+        public object Clone()
+        {
+            return new Student();
+        }
+    }
+}
