46fbdf410d5c87a43317f2a31b3ece0caeb94bab
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 14:59:22 +0200

    finished Exercise5

---
+++ 6.week/day3/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var freq =
+                from number in n
+                group number by number into x
+                select x;
+
+            foreach (var num in freq)
+            {
+                Console.WriteLine("Number " + num.Key + " appears " + num.Count()+ " times" );
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
