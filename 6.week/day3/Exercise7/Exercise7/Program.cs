0b9c91bf3f568141a67de0a4bc73fe661c42db5f
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 16:12:46 +0200

    finished Exercise7

---
+++ 6.week/day3/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var result =
+                from city in cities
+                where city[0] == 'A' && city[city.Length - 1] == 'I' 
+                select city;
+
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
