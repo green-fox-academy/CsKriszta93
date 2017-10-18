4024d57fb7eefd866102bf99af81c8b2604ae299
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 15:22:33 +0200

    finished Exercise6

---
+++ 6.week/day3/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blabla = "sbfgsjfkjkfjkfsghfb";
+
+            var freq =
+                from i in blabla
+                group i by i into j
+                select j;
+
+            foreach (var letter in freq)
+            {
+                Console.WriteLine("The letter " + letter.Key + " appears " + letter.Count() + " times." );
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
