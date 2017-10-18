969b54d4e6da5b219d6b07a3a23ce238dc0c4b4c
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 13:47:55 +0200

    finished Exercise4

---
+++ 6.week/day3/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+            Console.WriteLine(string.Join(", ", SquareMoreThan20(n)));
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<int> SquareMoreThan20(int[] input)
+        {
+            var square =
+                from number in input
+                let sq = number * number
+                where sq > 20
+                select sq;
+            return square;
+        }
+    }
+}
