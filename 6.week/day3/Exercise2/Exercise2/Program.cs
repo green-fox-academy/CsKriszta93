d598207f25f0634bb0e238ea1f4cb21d25faf29d
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 12:40:32 +0200

    Exercise2 with lambda

---
+++ 6.week/day3/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var oddList = GetOddNumbers(n);
+            double result = oddList.Average();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static List<int> GetOddNumbers(int[] input)
+        {
+            var oddList = input.Where(x => x % 2 != 0).ToList();
+            return oddList;
+        } 
+    }
+}
