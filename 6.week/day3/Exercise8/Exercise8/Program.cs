338ebcfc1ddef68bcebccedfcda4647de583b164
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 16:29:51 +0200

    finished Exercise8 with lambda

---
+++ 6.week/day3/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blabla = "jLkdkXdkDkdkOPlS";
+            GetUppercaseLetters(blabla);
+            Console.ReadLine();
+        }
+
+        public static void GetUppercaseLetters(string input)
+        {
+            var letters = input.Where(x => char.IsUpper(x)).ToList();
+
+            foreach (var letter in letters)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
