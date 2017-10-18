ab64d8e7bd5f2a5164ea15eb6b8bb96183ac5dc9
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 13:26:53 +0200

    Exercise3 finsihed

---
+++ 6.week/day3/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            Console.WriteLine(string.Join(" , ", GetPositiveNumbers(n)));
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<int> GetPositiveNumbers(int[] input)
+        {
+            var oddNums =
+                from number in input
+                where number > 0
+                select number * number;
+            return oddNums;
+        }   
+    }
+}
