c8929a6aa4ee020a969ddcb84e7f2283598d4445
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-18 12:12:38 +0200

    finished Exercise1 with lambda

---
+++ 6.week/day2/Comparable/Comparable/Domino.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparable
+{
+    public interface IComparable
+    {
+        int CompareTo(object domino);
+    }
+   
+    public class Domino : IComparable
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+
+        public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+            return dominoes;
+        }
+
+        public int CompareTo()
+        {
+            return 
+        }
+    }
+}
---
+++ 6.week/day3/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            Console.WriteLine(string.Join(", ", GetEvenNumbers(n)));
+            GetEvenLambda(n); 
+            Console.ReadLine();       
+        }
+
+        public static IEnumerable<int> GetEvenNumbers(int[] input)
+        {
+            var evenArr =
+                (from number in input
+                where number % 2 == 0
+                select number);
+            return evenArr;
+        }
+
+        public static void GetEvenLambda(int[] input)
+        {
+            var evenWithLambda = input.Where(x => x % 2 == 0).ToList();
+
+            foreach (int i in evenWithLambda)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
