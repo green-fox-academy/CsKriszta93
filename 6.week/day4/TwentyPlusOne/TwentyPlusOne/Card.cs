e469b8a5bd71a29caf7faa2c396ec7c8fa518837
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-19 16:00:57 +0200

    Enums created for Card Class

---
+++ 6.week/day4/TwentyPlusOne/TwentyPlusOne/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyPlusOne
+{
+    public class Card
+    {
+        public Color cardColor { get; set; }
+        public Suit cardSuit { get; set; }
+        public Rank cardRank { get; set; }
+
+        public enum Color
+        {
+            red,
+            black
+        }
+
+        public enum Suit
+        {
+            clubs,
+            diamonds,
+            hearts,
+            spades            
+        }
+
+        public enum Rank
+        {
+            two = 2,
+            three =3, 
+            four = 4, 
+            five = 5, 
+            six = 6, 
+            seven = 7, 
+            eight = 8, 
+            nine = 9, 
+            ten = 10,
+            J = 11,
+            Q = 12,
+            K = 13,
+            A = 14
+        }
+    }
+}
---
+++ 6.week/day4/TwentyPlusOne/TwentyPlusOne/Deck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyPlusOne
+{
+    class Deck
+    {
+        public void ShuffleDeck()
+        {
+
+        }
+
+        public void PullFirst()
+        {
+
+        }
+
+        public void PullLast()
+        {
+
+        }
+
+        public void PullRandom()
+        {
+
+        }
+    }
+}
