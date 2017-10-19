b7dfbfb70bdf9f03bc147b1090439537b89ef4a4
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-19 12:53:12 +0200

    Constructor created

--- 6.week/day3/ParkingLot/ParkingLot/Car.cs
+++ 6.week/day3/ParkingLot/ParkingLot/Car.cs
@@ -6,43 +6,36 @@ using System.Threading.Tasks;
 
 namespace ParkingLot
 {
-    public enum Types
-    {
-        Mazda,
-        Audi,
-        Maserati,
-        AlfaRomeo,
-        Mercedes,
-        Lamborghini,
-        FordMustang
-    }
-
-    public enum Colors
-    {
-        Red,
-        Black,
-        White,
-        Blue,
-        Grey
-    }
-
     public class Car
     {
-        public List<Car> cars = new List<Car>();
-        public Random random;
+        public Type CarTypes { get; set; }
+        public Color CarColors { get; set; }
+        public Random random = new Random();
 
-        public Car()
+        public enum Type
         {
-            random = new Random();
+            Mazda,
+            Audi,
+            Maserati,
+            AlfaRomeo,
+            Mercedes,
+            Lamborghini,
+            FordMustang
         }
 
-        public List<Car> AddCarsToList()
+        public enum Color
         {
-            var carsList = new List<Car>();
-            for (int i = 0; i < 256; i++)
-            {
-                carsList.Add(new Car());
-            }
+            Red,
+            Black,
+            White,
+            Blue,
+            Grey
+        }
+
+        public Car(Type carType, Color carColor)
+        {
+            this.CarTypes = carType;
+            this.CarColors = carColor;
         }
     }
 }
