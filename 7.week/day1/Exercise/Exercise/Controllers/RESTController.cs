96e54fc35f6618cf78d457c3ba9882514703c2af
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-24 17:02:13 +0200

    Greeting method and object

--- 7.week/day1/Exercise/Exercise/Controllers/RESTController.cs
+++ 7.week/day1/Exercise/Exercise/Controllers/RESTController.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Exercise.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Exercise.Controllers
 {
-    public class HomeController : Controller
+    [Route("api")]
+    public class RESTController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        [Route("greeting")]
+        public IActionResult Greeting()
         {
-            return View();
-        }
-
-        [Route("hello")]
-        public ContentResult Hello()
-        {
-            return Content("Hello World!");
+            Greeting greeting = new Greeting();
+            return new JsonResult(greeting);
         }
     }
 }
---
+++ 7.week/day1/Exercise/Exercise/Models/Greeting.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise.Models
+{
+    public class Greeting
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+    }
+}
