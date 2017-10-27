8ddcd2cd24a60fff0b51083ac4167e175613352d
CsKriszta93 <kriszta.csere93@gmail.com>
2017-10-27 09:07:31 +0200

    Trying to solve the Greeting exercise

---
+++ 7.week/day4/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetSomeone.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetSomeone.Controllers
+{
+    public class HomeController : Controller
+    {
+        Greet greet;
+
+        public HomeController(Greet greet)
+        {
+            this.greet = greet;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("greeting")]
+        public IActionResult Greet(string userName)
+        {
+            greet.Name = userName;
+            return RedirectToAction("GreetUser");
+        }
+
+        [HttpGet]
+        [Route("greeting")]
+        public IActionResult GreetUser()
+        { 
+            return View(greet);
+        }
+    }
+}
