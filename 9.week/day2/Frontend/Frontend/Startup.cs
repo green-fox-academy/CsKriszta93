1d4e073bfd620f2dc424092ac719cb97cf465589
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-07 11:40:19 +0100

    js and cshtml added to Frontend exercise

--- 9.week/day2/Frontend/Frontend/Startup.cs
+++ 9.week/day2/Frontend/Frontend/Startup.cs
@@ -16,6 +16,7 @@ namespace Frontend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +28,10 @@ namespace Frontend
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
