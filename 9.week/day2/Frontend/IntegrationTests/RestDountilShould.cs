50f873606184d8655d691f044a21e97e7c1b79aa
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-08 15:21:42 +0100

    working Dountil sum Xunit test

---
+++ 9.week/day2/Frontend/IntegrationTests/RestDountilShould.cs
@@ -0,0 +1,30 @@
+﻿using IntegrationTests.TestFixtures;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests
+{
+    [Collection("BaseCollection")]
+    public class RestDountilShould
+    {
+        private TestContext Context;
+
+        public RestDountilShould(TestContext context)
+        {
+            Context = context;
+        }
+
+        [Fact]
+        public async Task ReturnOkStatus()
+        {
+            var response = await Context.Client.GetAsync("/rest/dountil?what=sum,number=15");
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
