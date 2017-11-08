97dd3bedd59a076a3953411dac871843e35d4cd9
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-08 15:11:40 +0100

    working AppendA Xunit test

---
+++ 9.week/day2/Frontend/IntegrationTests/RestAppendAShould.cs
@@ -0,0 +1,30 @@
+﻿using IntegrationTests.TestFixtures;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests
+{
+    [Collection("BaseCollection")]
+    public class RestAppendAShould
+    {
+        private TestContext Context;
+
+        public RestAppendAShould(TestContext context)
+        {
+            Context = context;               
+        }
+
+        [Fact]
+        public async Task ReturnOkStatus()
+        {
+            var response = await Context.Client.GetAsync("/rest/appenda?appendable=kuty");
+            string responseJson = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
