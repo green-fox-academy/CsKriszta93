2e22e89e3ae9feb0c77c1281ed250729191f0d03
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-15 16:19:56 +0100

    Collection and TestContext classes added

--- 10.week/CalorieTableIntegrationTest/TestFixture/TestContext.cs
+++ 10.week/CalorieTableIntegrationTest/TestFixture/TestContext.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using CalorieTable;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace CalorieTableIntegrationTest.TestFixture
 {
-    class TestContext
+    public class TestContext
     {
+        public HttpClient Client { get; set; }
+        private readonly TestServer Server;
+
+        public TestContext()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
     }
 }
