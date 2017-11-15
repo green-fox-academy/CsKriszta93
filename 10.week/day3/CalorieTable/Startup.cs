e0f36d27a2011e98931cd8fa9047d585261c704d
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-15 13:47:06 +0100

    appsettings.json file added

--- 10.week/day3/CalorieTable/Startup.cs
+++ 10.week/day3/CalorieTable/Startup.cs
@@ -10,16 +10,26 @@ using Microsoft.Extensions.Logging;
 using CalorieTable.Repositories;
 using CalorieTable.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace CalorieTable
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
             services.AddMvc();
             services.AddScoped<FoodRepository>();
-            services.AddDbContext<FoodContext>(options => options.UseSqlServer());
+            services.AddDbContext<FoodContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:FoodConnection"]));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
