0f89831d6da208bda00c4ff2d6d3e92b45264a4b
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-15 12:32:02 +0100

    Get tasks of student function created

--- 10.week/day1/FoxManager/Controllers/HomeController.cs
+++ 10.week/day1/FoxManager/Controllers/HomeController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using FoxManager.Repositories;
 
 namespace FoxManager.Controllers
 {
+    [Route("home")]
     public class HomeController : Controller
     {
+        FoxRepository FoxRepository;
+
+        public HomeController(FoxRepository foxRepository)
+        {
+            FoxRepository = foxRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
--- 10.week/day1/FoxManager/Repositories/FoxRepository.cs
+++ 10.week/day1/FoxManager/Repositories/FoxRepository.cs
@@ -1,4 +1,5 @@
 ﻿using FoxManager.Entities;
+using FoxManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace FoxManager.Repositories
             FoxContext = foxContext;
         }
 
-        public List<Models.Task> GetTaks()
+        public List<Models.Task> GetTasks()
         {
             return FoxContext.Tasks.ToList();
         }
@@ -24,6 +25,25 @@ namespace FoxManager.Repositories
         {
             FoxContext.Tasks.Add(new Models.Task { TaskName = taskname });
             FoxContext.SaveChanges();
+        }
+
+        public bool CheckStudentExists(string name)
+        {
+            var checkstudent = FoxContext.Students.FirstOrDefault(student => student.Name.Equals(name));
+            return checkstudent != null ? false : true;
+        }
+
+        public Student StudentInfo(string name)
+        {
+            var selectedstudent = (from student in FoxContext.Students
+                                   where student.Name == name
+                                   select student).FirstOrDefault();
+            return selectedstudent;
+        }
+
+        public List<Models.Task> GetStudentTasks(string name)
+        {
+            return FoxContext.Tasks.Where(y => y.Student.Name.Equals(name)).ToList();
         }
     }
 }
